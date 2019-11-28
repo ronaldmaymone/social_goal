5c8aba0b7969475bc547c9a978eff0c3792cf5b9
ArtV97 <51306971+ArtV97@users.noreply.github.com>
2019-11-28 12:28:40 -0300

    Followed Page Ok

--- lib/pages/followedgoalspage.dart
+++ lib/pages/followedgoalspage.dart
@@ -11,54 +11,66 @@ class FollowedGoalPage extends StatefulWidget {
 }
 
 class _FollowedGoalPageState extends State<FollowedGoalPage> {
-  List<DocumentSnapshot> docs = List();
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document){
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
-      child: Column(
-        children: <Widget>[
-          Image.network(document["ImgPath"]),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(document["Title"],style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
-                Text("Created by ${document["CreatorName"]}",
-                    style: TextStyle(fontStyle: FontStyle.italic,fontSize: 10.0)),
-                Divider(),
-                Text(document["Description"])  // TEXT DESCRIPTION FROM ABOVE
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Future<void> getDocsSnap() async {
-    if (widget.user.followedGoals.length >= 1) {
-      debugPrint("RETORNEI MAIS OU IGUAL A 1 FOLLOWED GOAL");
-      for (String docId in widget.user.followedGoals) {
-        DocumentSnapshot ref = await Firestore.instance.collection("Goals").document(docId).get();
-        debugPrint("Referência = "+ref.toString());
-        docs.add(ref);
-      }
+    if (widget.user.followedGoals.contains(document.documentID)){
+      return Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+        child: Column(
+          children: <Widget>[
+            Image.network(document["ImgPath"]),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(document["Title"],style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
+                  Text("Created by ${document["CreatorName"]}",
+                      style: TextStyle(fontStyle: FontStyle.italic,fontSize: 10.0)),
+                  Divider(),
+                  Text(document["Description"])  // TEXT DESCRIPTION FROM ABOVE
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    else{
+      return Container();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    getDocsSnap();
+    //getDocsSnap();
     return Scaffold(
         body:
-        ListView.builder(
-          //itemExtent: 80.0,
-          itemCount: widget.user.followedGoals.length,
-          itemBuilder: (context, index) => widget.user.followedGoals.length == 0 ? Center(child: Text("Você ainda não segue nenhum objetivo")) :
-          _buildListItem(context, docs[index]),
-        )
+        new StreamBuilder(
+          stream: Firestore.instance.collection("Goals").snapshots(),
+          builder: (context, snapshot) {
+              if (!snapshot.hasData){
+                return Scaffold(
+                  body: Container(
+                  alignment: Alignment.center,
+                    child: Column(
+                      children: <Widget>[
+                        Text("Loading Objectives",style: TextStyle(fontWeight: FontWeight.bold)),
+                        CircularProgressIndicator()
+                            ],
+                          )
+                      )
+                );
+              }
+              else {
+                return ListView.builder(
+                //itemExtent: 80.0,
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) =>
+                    _buildListItem(context, snapshot.data.documents[index])
+                );
+              }
+            }),
     );
   }
 }
